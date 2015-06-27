1892187bb858e6dc033bedcc7a2c65760bc00286
kanaima <ferrarijuanp@gmail.com>
2015-06-27 17:13:43 -0300

    server configuration

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var config = require('./config/config');
+//var server = require('./app/server');
+var httpServer = require('http');
+
+httpServer.listen(3000, function(){
+  console.log('Listen Port'+ 3000);
+});
