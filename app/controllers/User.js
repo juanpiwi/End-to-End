4c5899c361f4e63f815d52e1ba10fd93a1af0def
Juan Pablo Ferrari <ferrarijuanp@gmail.com>
2015-08-30 21:44:10 -0300

    CRUD user

--- app/controllers/User.js
+++ app/controllers/User.js
@@ -6,32 +6,100 @@ var userController = {};
 
 (function(){
 
-  this.create = function(req, res, next){
-    if(req.body.user === undefined || req.body.password === undefined || req.body.user === '' || req.body.password === ''){
-      return res.send('Hay campos vacios');
-    }
-    var user = new User(req.body);
-    user.save(function(err){
-
+  //GET - Return all users in the DB
+  this.findAll = function (req, res){
+    User.find(function(err, users){
       if(err){
-        return next(err);
-      }else{
-        res.json(user);
+        res.send(500, err.message);
       }
+      console.log('GET /users');
+      res.status(200).jsonp(users);
     });
   };
 
-  this.read = function(req, res, next){
-
-    User.find({}, function(err, data){
+  //GET - Return a User with specified ID
+  this.findById = function (req, res){
+    User.findById(req.params.id, function(err, users){
       if(err){
-        return next(err);
-      }else{
-        res.json(data);
+        res.send(500, err.message);
       }
+      console.log('GET /users/',req.params.id);
+      res.status(200).jsonp(users);
+    });
+  };
+
+  //POST - Insert a new User in the DB
+  this.add = function(req, res) {
+    console.log('POST');
+    console.log(req.body);
+
+    var user = new User({
+      user:    req.body.user,
+      password: 	  req.body.password
     });
 
+    user.save(function(err, user) {
+      if(err) {
+        return res.send(500, err.message);
+      }
+      res.status(200).jsonp(user);
+    });
   };
+
+  //PUT - Update a register already exists
+  this.update = function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+      user.user   = req.body.user;
+      user.password    = req.body.password;
+
+      user.save(function(err) {
+        if(err){
+          return res.send(500, err.message);
+        }
+        res.status(200).jsonp(user);
+      });
+    });
+  };
+
+  //DELETE - Delete a User with specified ID
+  this.delete = function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+      user.remove(function(err) {
+        if(err) {
+          return res.send(500, err.message);
+        }
+        res.status(200);
+      })
+    });
+  };
+
+
+  /*this.create = function(req, res, next){
+   if(req.body.user === undefined || req.body.password === undefined || req.body.user === '' || req.body.password === ''){
+   return res.send('Hay campos vacios');
+   }
+   var user = new User(req.body);
+   user.save(function(err){
+
+   if(err){
+   return next(err);
+   }else{
+   res.json(user);
+   }
+   });
+   };
+
+   this.read = function(req, res, next){
+
+   User.find({}, function(err, data){
+   if(err){
+   return next(err);
+   }else{
+   res.json(data);
+   }
+   });
+
+   };*/
 
 }).apply(userController)
 
--- app/router.js
+++ app/router.js
@@ -8,14 +8,26 @@ var userController = require('../app/controllers/User');
 /**
  * Method that contains the HTTP method GET for the root of the app
  */
+/*
 router.get('/', function(req, res){
   res.sendStatus(403);
 });
-
+*/
+/*
 router.post('/register', personaController.post);
 
 router.post('/users', userController.create);
 router.get('/users/list', userController.read);
+*/
+
+router.route('/users')
+  .get(userController.findAll)
+  .post(userController.add);
+
+router.route('/users/:id')
+  .get(userController.findById)
+  .put(userController.update)
+  .delete(userController.delete);
 
 
 
