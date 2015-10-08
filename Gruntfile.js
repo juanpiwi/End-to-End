e279a83733509966dd7a0fd85e0818e8844e460e
Juan Pablo Ferrari <ferrarijuanp@gmail.com>
2015-10-08 17:51:11 -0300

    karma - upgrade & configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,42 @@
 module.exports = function(grunt) {
 
-    /***** CONFIGURATION *****/
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  /***** CONFIGURATION *****/
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        // Generate JS documentation
-            jsdoc: {
-            frontend: {
-                options: {
-                    destination: './doc/frontend',
-                    template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
-                    configure: './doc/jsdoc.frontend.conf.json'
-                },
-                src: ['./public/app/**/*.js']
-            },
-            backend: {
-                options: {
-                    destination: './doc/backend',
-                    template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
-                    configure: './doc/jsdoc.backend.conf.json'
-                },
-                src: ['./app/**/*.js', './index.js', 'README.md']
-            }
+    // Generate JS documentation
+    jsdoc: {
+      /*frontend: {
+          options: {
+              destination: './doc/frontend',
+              template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
+              configure: './doc/jsdoc.frontend.conf.json'
+          },*/
+      //src: ['./public/app/**/*.js']
+      //},
+      backend: {
+        options: {
+          destination: './doc/backend',
+          template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
+          configure: './doc/jsdoc.backend.conf.json'
         },
-        karma: {
-            unit: {
-                configFile: 'karma.conf.js'
-            }
-        }
+        src: ['./app/**/*.js', './index.js', 'README.md']
+      }
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    }
 
-    });
-
-
-    /***** LOAD TASKS *****/
-    grunt.loadNpmTasks('grunt-jsdoc');
-    grunt.loadNpmTasks('grunt-karma');
+  });
 
 
-    /***** REGISTER TASKS *****/
+  /***** LOAD TASKS *****/
+  grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-karma');
+
+
+  /***** REGISTER TASKS *****/
 
 }
